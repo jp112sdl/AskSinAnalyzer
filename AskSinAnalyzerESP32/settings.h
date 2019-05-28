66ae0b3beaf1bc8e65a2c26b7ff1eb9e70ecc9ce
Jérôme <jp112sdl@icloud.com>
2019-05-28 10:02:11 +0200

    Update settings.h

--- AskSinAnalyzerESP32/settings.h
+++ AskSinAnalyzerESP32/settings.h
@@ -4,7 +4,7 @@
 #define WIFI_SSID "MyWLAN"
 #define WIFI_PSK  "MyPSK"
 
-const String CCU_IP    = "192.168.1.123;
+const String CCU_IP    = "192.168.1.123";
 const String INPUT_SV  = "Analyzer_Input";
 const String OUTPUT_SV = "Analyzer_Output";
 
