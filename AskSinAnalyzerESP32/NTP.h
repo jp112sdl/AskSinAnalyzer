4454db848def9e5a2496712d433a7120ef6f8aca
jp112sdl <jp112sdl@icloud.com>
2019-05-30 14:54:29 +0200

    add time functions

---
+++ AskSinAnalyzerESP32/NTP.h
@@ -0,0 +1,88 @@
+const int NTP_PACKET_SIZE = 48;
+byte packetBuffer[ NTP_PACKET_SIZE];
+unsigned int localPort = 2390;
+char ntpServerName[50] = "0.de.pool.ntp.org";
+WiFiUDP udp;
+
+unsigned long sendNTPpacket(IPAddress& address) {
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  packetBuffer[0] = 0b11100011;
+  packetBuffer[1] = 0;
+  packetBuffer[2] = 6;
+  packetBuffer[3] = 0xEC;
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+  udp.beginPacket(address, 123);
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
+}
+
+time_t getNtpTime() {
+  IPAddress ntpServerIP;
+  while (udp.parsePacket() > 0) ;
+  WiFi.hostByName(ntpServerName, ntpServerIP);
+  sendNTPpacket(ntpServerIP);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1600) {
+    int size = udp.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      udp.read(packetBuffer, NTP_PACKET_SIZE);
+      unsigned long secsSince1900;
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      Serial.println("NTP time was set");
+      return secsSince1900 - 2208988800UL;
+    }
+  }
+  return 0;
+}
+
+bool doNTPinit() {
+  udp.begin(localPort);
+  setSyncProvider(getNtpTime);
+  setSyncInterval(3600);
+  byte timeSetRetries = 0;
+  while (timeStatus() == timeNotSet) {
+    Serial.println("Waiting for Time set");
+    delay(2000);
+    if (timeSetRetries > 5) {
+      Serial.println("Timeout!"); 
+      //Serial.println("Rebooting");
+      //delay(2000);
+      //ESP.restart();
+      return false;
+    }
+    timeSetRetries++;
+  }
+  return true;
+}
+
+boolean summertime(time_t t, byte tzHours) {
+  if (month(t) < 3 || month(t) > 10) return false; // keine Sommerzeit in Jan, Feb, Nov, Dez
+  if (month(t) > 3 && month(t) < 10) return true; // Sommerzeit in Apr, Mai, Jun, Jul, Aug, Sep
+  if (month(t) == 3 && (hour(t) + 24 * day(t)) >= (1 + tzHours + 24 * (31 - (5 * year(t) / 4 + 4) % 7)) || month(t) == 10 && (hour(t) + 24 * day(t)) < (1 + tzHours + 24 * (31 - (5 * year(t) / 4 + 1) % 7)))
+    return true;
+  else
+    return false;
+}
+
+String getUhrzeit(time_t t) {
+  byte Stunde = hour(t) + 1;
+  Stunde = (summertime(t, 0)) ? Stunde + 1 : Stunde;
+  String Minute = (minute(t) < 10) ? "0" + String(minute(t)) : String(minute(t));
+  String Sekunde = (second(t) < 10) ? "0" + String(second(t)) : String(second(t));
+  return String(Stunde) + ":" + Minute + ":" + Sekunde;
+}
+
+String getDatum(time_t t) {
+  String Tag = (day(t) < 10) ? "0" + String(day(t)) : String(day(t));
+  String Monat = (month(t) < 10) ? "0" + String(month(t)) : String(month(t));
+  String Jahr = String(year(t));
+  return Tag + "." + Monat + "." + Jahr;
+}
+
+
