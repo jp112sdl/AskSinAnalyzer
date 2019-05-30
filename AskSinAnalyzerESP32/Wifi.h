24d28e11564fce8974dcbfbdd2bc7d5f92b2a3b0
jp112sdl <jp112sdl@icloud.com>
2019-05-30 14:55:42 +0200

    print wifi details after connected

--- AskSinAnalyzerESP32/Wifi.h
+++ AskSinAnalyzerESP32/Wifi.h
@@ -1,18 +1,32 @@
 #ifndef WIFIFUNCTIONS_H_
 #define WIFIFUNCTIONS_H_
 
+void printWifiStatus() {
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+  IPAddress IP = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(IP);
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
 bool wifiConnect() {
   drawStatusCircle(ILI9341_YELLOW);
   WiFi.begin(WIFI_SSID, WIFI_PSK);
   uint8_t connect_count = 0;
+  Serial.println("Connecting to WiFi.");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.println("Connecting to WiFi..");
+    Serial.print(".");
     connect_count++;
     if (connect_count > 60) return false;
   }
   connect_count = 0;
-  Serial.println("Connected to the WiFi network");
+  Serial.println("\nConnected to the WiFi network");
+  printWifiStatus();
   return true;
 }
 
