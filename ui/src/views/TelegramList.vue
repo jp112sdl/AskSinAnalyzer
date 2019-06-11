87d6ccb9b65bc5a5ec3bda1dbfb10a688bd7c157
Christoph Wiechert <wio@psitrax.de>
2019-06-11 14:17:56 +0200

    UI: Add TelegramListView

---
+++ ui/src/views/TelegramList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <telegram-list :value="$root.data.telegrams"/>
+  </div>
+</template>
+
+<script>
+
+  import TelegramList from '@/components/TelegramList.vue'
+
+  export default {
+  name: 'TelegramListView',
+  components: {
+    TelegramList,
+  }
+}
+</script>
