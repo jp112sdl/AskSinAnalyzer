91a82e42f16cfc8421a09b2421259c0f8f246524
Christoph Wiechert <wio@psitrax.de>
2019-06-11 14:24:44 +0200

    UI: Remove pi color gradients

--- ui/src/components/PieChart.vue
+++ ui/src/components/PieChart.vue
@@ -39,22 +39,6 @@
     },
 
     mounted() {
-      Highcharts.setOptions({
-        colors: Highcharts.map(Highcharts.getOptions().colors, function(color) {
-          return {
-            radialGradient: {
-              cx: 0.5,
-              cy: 0.3,
-              r: 0.7
-            },
-            stops: [
-              [0, color],
-              [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
-            ]
-          };
-        })
-      });
-
       this.hightchart = Highcharts.chart(this.$refs.chart, {
         chart: {
           plotBackgroundColor: null,
