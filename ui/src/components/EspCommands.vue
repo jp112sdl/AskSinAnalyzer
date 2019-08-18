c27e7870bea8665168bb1fa27301f28659af7e0f
Christoph Wiechert <wio@psitrax.de>
2019-08-18 19:19:23 +0200

    Disable CSV-Buttons when sdcard is not available

--- ui/src/components/EspCommands.vue
+++ ui/src/components/EspCommands.vue
@@ -28,7 +28,7 @@
           <q-item-label caption>Startet den ESP im Config-Modus neu.</q-item-label>
         </q-item-section>
       </q-item>
-      <q-item clickable v-ripple @click="downloadcsv">
+      <q-item clickable v-ripple @click="downloadcsv" :disabled="!$root.espConfig.sdcardavailable">
         <q-item-section avatar>
           <q-avatar rounded color="primary" text-color="white" icon="save_alt"/>
         </q-item-section>
@@ -37,7 +37,7 @@
           <q-item-label caption>CSV mit empfangenen Telegrammen herunterladen.</q-item-label>
         </q-item-section>
       </q-item>
-      <q-item clickable v-ripple @click="deletecsv">
+      <q-item clickable v-ripple @click="deletecsv" :disabled="!$root.espConfig.sdcardavailable">
         <q-item-section avatar>
           <q-avatar rounded color="primary" text-color="white" icon="delete_sweep"/>
         </q-item-section>
