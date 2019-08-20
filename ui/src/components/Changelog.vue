4442033cba8611592bbccc0cc40bbdb5eb6d6fd0
Christoph Wiechert <wio@psitrax.de>
2019-08-21 00:30:35 +0200

    UI: Some styles

--- ui/src/components/Changelog.vue
+++ ui/src/components/Changelog.vue
@@ -17,6 +17,10 @@
 </script>
 
 
-<style lang="stylus">
-
+<style lang="stylus" scoped>
+  h2
+    font-size 1.4rem
+    font-weight bold
+    border-bottom 1px solid #c2c0c0
+    line-height 1.5rem
 </style>
