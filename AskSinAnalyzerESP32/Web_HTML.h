e8d5f988271277613b7dc865393390eff21bf6fd
HMSteve <HMSteve@users.noreply.github.com>
2019-09-09 20:59:32 +0200

    Update Web_HTML.h

--- AskSinAnalyzerESP32/Web_HTML.h
+++ AskSinAnalyzerESP32/Web_HTML.h
@@ -14,11 +14,11 @@ const char HTTP_INDEX[] PROGMEM = R"=====(
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="msapplication-TileColor" content="#027be3">
     <meta name="theme-color" content="#027be3">
-    <link rel="apple-touch-icon" sizes="180x180" href="https://jp112sdl.github.io/AskSinAnalyzer/{branch}/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="https://jp112sdl.github.io/AskSinAnalyzer/{branch}/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="https://jp112sdl.github.io/AskSinAnalyzer/{branch}/favicon-16x16.png">
-    <link rel="manifest" href="https://jp112sdl.github.io/AskSinAnalyzer/{branch}/site.webmanifest">
-    <link rel="mask-icon" href="https://jp112sdl.github.io/AskSinAnalyzer/{branch}/safari-pinned-tab.svg" color="#027be3">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://HMSteve.github.io/AskSinAnalyzer/{branch}/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://.github.io/AskSinAnalyzer/{branch}/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://HMSteve.github.io/AskSinAnalyzer/{branch}/favicon-16x16.png">
+    <link rel="manifest" href="https://HMSteve.github.io/AskSinAnalyzer/{branch}/site.webmanifest">
+    <link rel="mask-icon" href="https://HMSteve.github.io/AskSinAnalyzer/{branch}/safari-pinned-tab.svg" color="#027be3">
   </head>
   <body>
   <noscript>
@@ -40,11 +40,11 @@ const char HTTP_INDEX[] PROGMEM = R"=====(
         + "Internet Einstellungen und Werbeblocker überprüfen.</p>"
     }
     var scriptEl = document.createElement('script');
-    scriptEl.src = "https://jp112sdl.github.io/AskSinAnalyzer/{branch}/js/app.js";
+    scriptEl.src = "https://HMSteve.github.io/AskSinAnalyzer/{branch}/js/app.js";
     scriptEl.onerror = logErr;
     document.head.append(scriptEl);
     var cssEl = document.createElement('link');
-    cssEl.href = "https://jp112sdl.github.io/AskSinAnalyzer/{branch}/css/app.css";
+    cssEl.href = "https://HMSteve.github.io/AskSinAnalyzer/{branch}/css/app.css";
     cssEl.rel = "stylesheet";
     cssEl.onerror = logErr;
     document.head.append(cssEl);
